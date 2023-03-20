f440ac528b0f626f74fbd546cd46c08fda872309
Teshiification <panini62@web.de>
2023-03-20 19:59:53 +0100

    feat(style): scrollbar and projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,62 +7,10 @@ export default function RootLayout({
 }: {
     children: React.ReactNode
 }) {
-    const pages = [
-        {
-            link: '/',
-            title: 'Home',
-            button: false,
-        },
-        {
-            link: '/about',
-            title: 'About me',
-            button: false,
-        },
-        {
-            link: '/projects',
-            title: 'Projects',
-            button: false,
-        },
-        {
-            link: '/contact',
-            title: 'Hire me',
-            button: true,
-        },
-    ]
     return (
         <html>
-            <body className="grid grid-rows w-screen h-screen overflow-x-hidden bg-body z-20">
-                <nav className="absolute grid justify-around w-1/4 h-screen right-40 top-0 bottom-0 bg-vicious-white bg-opacity-10 rounded my-auto py-4 z-20">
-                    <div className="grid gap-10 h-1/2 my-auto text-center font-semibold uppercase">
-                        {pages.map((item, key) => {
-                            if (!item.button)
-                                return (
-                                    <Link
-                                        key={key}
-                                        href={item.link}
-                                        className="group text-vicious-secondary hover:text-vicious-primary mt-2 cursor-pointer h-fit"
-                                    >
-                                        {item.title}
-                                        <div className="h-[1px] bg-vicious-white bg-opacity-60 group-hover:bg-opacity-40 rounded w-80"></div>
-                                    </Link>
-                                )
-                            else
-                                return (
-                                    <Link
-                                        key={key}
-                                        href={item.link}
-                                        className="h-10 text-vicious-secondary bg-vicious-primary bg-opacity-80 rounded-[10px] p-2 shadow-3xl hover:bg-opacity-100 hover:scale-105 cursor-pointer"
-                                    >
-                                        {item.title}
-                                    </Link>
-                                )
-                        })}
-                    </div>
-                </nav>
+            <body className="bg-body w-screen overflow-x-hidden scrollbar-thumb-vicious-primary scrollbar scrollbar-thumb-rounded-full scrollbar-track-vicious-black">
                 {children}
-                <footer className="flex absolute bottom-0 w-full h-10 justify-around">
-                    <Link href="/impressum">Impressum</Link>
-                </footer>
             </body>
         </html>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,122 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import Seperator from '../components/seperator'
 
 export default function Index() {
+    const pages = [
+        {
+            link: '/',
+            title: 'Home',
+        },
+        {
+            link: '#about',
+            title: 'About me',
+        },
+        {
+            link: '#projects',
+            title: 'Projects',
+        },
+    ]
+
+    const projects = [
+        {
+            title: 'Portfolio 2023',
+            text: 'My new Portfolio',
+            link: '',
+            type: 'github',
+        },
+        {
+            title: 'Portfolio 2020 - 2023',
+            text: 'My old Portfolio',
+            link: '',
+            type: 'github',
+        },
+        {
+            title: 'Portfolio Marcel Sturm',
+            text: 'Customer Portfolio',
+            link: '',
+            type: 'github',
+        },
+    ]
+
     return (
-        <>
+        <div className="grid gap-16">
             <section
                 id="hero"
-                className="py-32 h-screen w-screen bg-vicious-black "
+                className="h-screen pt-10 bg-vicious-black bg-opacity-60"
                 style={{
                     backgroundImage: `url(/images/portraits/landscape.jpg)`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
-                    backgroundBlendMode: 'lighten',
+                    backgroundBlendMode: 'multiply',
                 }}
             >
-                <div className="container left-0 my-80">
-                    <div className="w-1/3 text-center font-bold ml-60">
-                        <h1 className="text-6xl text-vicious-white my-4">
-                            Danny Sinicco
-                        </h1>
-                        <p className="text-xl text-vicious-white mx-8 mb-8">
-                            Designer & Programmierer
-                        </p>
-                        <h2 className="text-2xl text-vicious-white mx-8 mb-8">
-                            Willkommen auf meinem Portfolio
-                        </h2>
+                <div
+                    id="navbar"
+                    className="absolute hidden lg:grid w-1/4 h-screen lg:right-20 top-0 bottom-0 bg-vicious-white bg-opacity-10 justify-around text-center font-semibold uppercase items-center"
+                >
+                    <div className="grid gap-10">
+                        {pages.map((item, key) => {
+                            return (
+                                <Link
+                                    key={key}
+                                    href={item.link}
+                                    className="group text-vicious-secondary hover:text-vicious-primary cursor-pointer"
+                                >
+                                    {item.title}
+                                    <div className="h-[1px] bg-vicious-white bg-opacity-60 group-hover:bg-opacity-40 rounded w-40"></div>
+                                </Link>
+                            )
+                        })}
                     </div>
+                    <Link
+                        href={'#job'}
+                        className="h-8 w-48 text-vicious-secondary border-2 border-vicious-primary bg-white bg-opacity-20 rounded shadow-3xl hover:bg-opacity-40 cursor-pointer"
+                    >
+                        Hire me
+                    </Link>
+                </div>
+                <div className="grid left-0 my-20 ml-10 w-1/3 text-center font-bold gap-4">
+                    <h1 className="text-6xl text-vicious-white my-4">
+                        Danny Sinicco
+                    </h1>
+                    <p className="text-xl text-vicious-white mx-8 mb-8">
+                        Design & Programming
+                    </p>
                 </div>
             </section>
-        </>
+
+            <section id="about" className="h-1/2">
+                <div
+                    id="container"
+                    className="grid lg:flex justify-between bg-vicious-gray lg:w-1/2 mx-auto rounded p-4"
+                >
+                    <div
+                        id="img_container_gradient"
+                        className="p-1 shrink-0 rounded bg-gradient-to-br from-vicious-primary to-vicious-secondary w-48 h-48"
+                    >
+                        <div
+                            id="img_container"
+                            className="rounded w-full h-full"
+                            style={{
+                                backgroundImage: `url(/images/portraits/me-zoomed.jpg)`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                            }}
+                        />
+                    </div>
+                    <p className="text-vicious-white p-4 w-full h-full">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Culpa quibusdam itaque autem earum sint molestias
+                        cum sed id. Eius quam eligendi dolor eveniet sed enim ut
+                        quibusdam ipsam eos culpa.
+                    </p>
+                </div>
+            </section>
+            <section id="projects" className="h-screen">
+                projects
+            </section>
+        </div>
     )
 }
